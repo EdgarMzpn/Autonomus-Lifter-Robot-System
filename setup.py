331b7efafa66d9e933407acf8fff335f5d3bb86b
Ryuudai2610 <jesus.e.rdz@outlook.com>
2024-04-24 15:57:40 -0600

    Implemented the Lidar A1 in ROS2

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'puzzlebot_challenge'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+        ('share/' + package_name + '/config', glob('config/*')),
+        ('share/' + package_name + '/rviz', glob('rviz/*')),
+        ('share/' + package_name + '/urdf', glob('urdf/*')),
+        ('share/' + package_name + '/models', glob('models/*')),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jesus',
+    maintainer_email='jesus@todo.todo',
+    description='The puzzlebot package that has everything related to the challenge',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'rplidar = puzzlebot_challenge.filter_scan:main',
+            # 'odometry = puzzlebot_challenge.localisation:main',
+            # 'joint_state_publisher = puzzlebot_challenge.JointStatePublisher:main',
+            # 'velocity_control = puzzlebot_challenge.velocity_control:main'
+        ],
+    },
+)
